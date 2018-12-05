a290ac4d199e3b669160913d1eb8f951a2f2e804
mikeyrayvon <mikey@interglobal.vision>
2018-12-05 17:01:09 -0600

    add archive-thumb image size

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -9,4 +9,5 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'opengraph', 1200, 630, true );
 
   add_image_size( 'gallery', 1200, 9999, false );
+  add_image_size( 'archive-thumb', 424, 283, false);
 }
