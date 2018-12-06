0eeaf4fdb2e167d3071b6d9474c5040d1d7c4ec9
mikeyrayvon <mikey@interglobal.vision>
2018-12-05 21:57:17 -0600

    scaffold page template

---
+++ page.php
@@ -0,0 +1,35 @@
+<?php
+get_header();
+?>
+
+<main id="main-content">
+  <section id="page">
+    <div class="container">
+      <div class="grid-row">
+
+<?php
+if (have_posts()) {
+  while (have_posts()) {
+    the_post();
+?>
+
+        <article <?php post_class('grid-item item-s-12'); ?> id="post-<?php the_ID(); ?>">
+          <div id="page-content">
+            <?php the_content(); ?>
+          </div>
+        </article>
+
+<?php
+  }
+}
+?>
+
+      </div>
+    </div>
+  </section>
+
+</main>
+
+<?php
+get_footer();
+?>
