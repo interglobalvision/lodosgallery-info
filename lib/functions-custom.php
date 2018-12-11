58e4584907342db8bded4db3e3908a8f89bc22e4
mikeyrayvon <mikey@interglobal.vision>
2018-12-10 18:12:08 -0600

    use group show in both langs

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -8,7 +8,7 @@ function list_artists($artists, $single = false) {
     $a = 1;
 
     if ($artist_count >= 8 && !$single) {
-      _e('[:en]group show[:es]exposición colectiva[:]');
+      echo 'group show';
     } else {
       foreach ($artists as $artist) {
         echo $artist;
