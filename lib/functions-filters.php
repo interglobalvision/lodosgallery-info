a62ff43dd3a41f31276d3661752c949403a3590b
mikeyrayvon <mikey@interglobal.vision>
2018-12-13 17:24:53 -0600

    remove css ver number to clear cache

--- lib/functions-filters.php
+++ lib/functions-filters.php
@@ -45,3 +45,12 @@ function add_lazysize_on_srcset($attr) {
 
 }
 add_filter('wp_get_attachment_image_attributes', 'add_lazysize_on_srcset');
+
+// remove wp version param from any enqueued scripts
+function vc_remove_wp_ver_css_js( $src ) {
+    if ( strpos( $src, 'ver=' . get_bloginfo( 'version' ) ) )
+        $src = remove_query_arg( 'ver', $src );
+    return $src;
+}
+add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
