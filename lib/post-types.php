8de2a218ebee4cac8ec7a29f0fe25e8306df8754
mikeyrayvon <mikey@interglobal.vision>
2021-07-18 15:43:54 +0200

    modify archive url

--- lib/post-types.php
+++ lib/post-types.php
@@ -190,7 +190,7 @@ function register_cpt_artist() {
     'show_in_nav_menus' => true,
     'publicly_queryable' => true,
     'exclude_from_search' => false,
-    'has_archive' => true,
+    'has_archive' => 'artists',
     'query_var' => true,
     'can_export' => true,
     'rewrite' => true,
