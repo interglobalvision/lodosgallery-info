4b9aecb401d08a49d2f22d25f878d56aeaea554e
mikeyrayvon <mikey@interglobal.vision>
2021-07-21 20:49:14 +0200

    smaller artist list margin

--- archive-artist.php
+++ archive-artist.php
@@ -17,7 +17,7 @@ if (have_posts()) {
   while (have_posts()) {
     the_post();
 ?>
-              <li class="margin-bottom-tiny">
+              <li class="margin-bottom-micro">
                 <a href="<?php the_permalink(); ?>" class="artist-hover"><?php the_title(); ?></a>
                 <div class="artist-image" style="background-image: url('<?php echo get_the_post_thumbnail_url($post, 'archive-thumb'); ?>')"></div>
               </li>
